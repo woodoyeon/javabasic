dc98edddfa906ae84807136c35857cd8214f0e37
woodoyeon <162427815+woodoyeon@users.noreply.github.com>
2024-03-06 18:05:50 +0900

    Add files via upload

---
+++ Printt01.java
@@ -0,0 +1,19 @@
+package sec04;
+
+public class Printt01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String myName = "홍길동";
+		int myAge = 20;
+		
+		System.out.println("이름 = " + myName);
+		System.out.println("나이 = " + myAge);
+		System.out.println();
+		System.out.print("이름 = "+ myName+"  ");
+		System.out.print("나이 = " + myAge );
+
+	}
+
+}
