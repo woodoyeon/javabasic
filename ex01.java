0f82bd8b84906e8d3b1a4ddd9a5676b4b21ef7ae
woodoyeon <162427815+woodoyeon@users.noreply.github.com>
2024-03-11 13:35:23 +0900

    Add files via upload

---
+++ ex01.java
@@ -0,0 +1,29 @@
+package chap04;
+
+public class ex01 {
+    public static void main(String[] args) {
+        String compileLang = "Java";
+        String scriptLang = "JavaScript";
+
+        System.out.println("나는 우도연 입니다.");
+        System.out.println(scriptLang + " Program");
+        System.out.println("나는 우도연 입니다.\n");
+
+        String program = "Program";
+        System.out.println(compileLang + " " + program);
+        System.out.println(scriptLang + " " + program + "\n");
+
+        int a = 10;
+        int b = 15;
+        System.out.println(a + " + " + b + " = " + a + b);
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
+
+
+
+
+
+
+
+
